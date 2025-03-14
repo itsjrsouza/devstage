fa73d494a651b3a770aad28fff130add3d9b4300
Junior Fernandes <nelson.junior@outlook.com>
2025-03-13 22:30:15 -0300

    commit2

--- index.js
+++ index.js
@@ -4,51 +4,68 @@ const app = document.getElementById("app")
 // guardandando o usuário
 const users = [
     {
-        email: 'teste@teste.com',
-        phone: '9999999999',
+        email: "teste@teste.com",
+        phone: "9999999999",
         ref: 100,
         refBy: null
     },
     {
-        email: 'tust@tust.com',
-        phone: '9999999999',
+        email: "tust@tust.com",
+        phone: "9999999999",
         ref: 200,
         refBy: 100
     },
     {
-        email: 'tost@tost.com',
-        phone: '99999999999',
+        email: "tost@tost.com",
+        phone: "99999999999",
         ref: 300,
         refBy: 200
     }
 ]
 
 const getUser = (userData) => {
-    return user.find((user) => {
+    return users.find((user) => {
         return user.email == userData.email
     })
 }
 
 // vai contar a quantidade de pessoas que se inscreveram pelo seu link
-const getTotalSubscribes = (userData) => {
-    const subs = user.filter((user) => {
+const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
         return user.refBy == userData.ref
     })
 
-    return subs.lenght
+    return subs.length
 } 
 
 // mostrando o convite
-const showInvited = (userData) => {
+const showInvite = (userData) => {
     app.innerHTML = `
-        <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled/>
+    <main>
+        <h3>Inscrição confirmada!</h3>
 
-         <div id="stats">
-            <h4>${getTotalSubscribes(userData)}</h4>  // vai retornar o número de inscritos pelo seu link
-            <p>Inscrições feitas</p>
+        <p>
+            Convide mais pessoas e concorra a prêmios!</br>
+            Compartilhe o link e acompanhe as inscrições:
+        </p>
 
-         </div>
+        <div class="input-group">
+            <label for="link">
+                <img src="./assets/link.svg" alt="link icon">
+            </label>
+        <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+        </div>
+    </main>
+
+    <section class="stats">
+        
+        <h4>${getTotalSubscribers(userData)}</h4> 
+        <p>Inscrições feitas</p>
+
+    </section>
     `
+
+    app.setAttribute('class', 'page-invite')
 }
 
 const saveUser = (userData) => {
@@ -58,37 +75,36 @@ const saveUser = (userData) => {
         ...userData,
 
 //vai gerar um número de referência aleatório para cada usuário
-        ref: Math.round(Math.random()*4000),
+        ref: Math.round(Math.random() * 4000),
         refBy: 100
     }
 
-    user.push(newUser)
+    users.push(newUser)
+    console.log(users)
     return newUser
 } 
 
 // pegar o formulário
-const formAction = () => { 
+const formAction = () => {
     const form = document.getElementById("form")
     form.onsubmit = (event) => {
         event.preventDefault()
-        const formData = new FormData (form)
+        const formData = new FormData(form)
 
 //criando um objeto 
         const userData = {  
-            email: formData.get('email'),
-            phone: formData.get('phone'),
-       }
+            email: formData.get("email"),
+            phone: formData.get("phone"),
+}
 
     const user = getUser(userData)
-        if(user){
-
+        if (user) {
 //encontrei o usuário
-            showInvited = (user)
-        }
+            showInvite = (user)
 //não encontrei, vou precisar cadastrar        
-        else {
+        } else {
             const newUser = saveUser (userData)
-            showInvited (newUser)
+            showInvite(newUser)
         }
     }
 }
@@ -96,19 +112,55 @@ const formAction = () => {
 const startApp = () => { // vai iniciar a aplicação
 
 const content = `
-    <form id="#form">
-        <input type="email" name="email" placeholder="E-mail" />
-        <input type="text" name="phone" placeholder="Telefone" />
-        <button>
-            Confirmar
-        </button>
-    </form>
+         <main>
+                <section class="about">
+                    <div class="section-header">
+                        <h2>Sobre o evento</h2>
+
+                        <span class="badge">AO VIVO</span>
+                    </div>
+
+                    <p>
+                        Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+                    </br> </br>Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+                    </p>
+                </section>
+
+                <section class="registration">
+                    <h2>Inscrição</h2>
+
+                    <form id="form">
+                        <div class="input-wrapper">
+                            <div class="input-group">
+                                <label for="email">
+                                    <img src="./assets/mail.svg" alt="email icon">
+                                </label>
+                                <input type="email" id="email" name="email" placeholder="E-mail"/>
+                             </div>
+                                
+                            <div class="input-group">
+                                <label for="phone">
+                                    <img src="./assets/phone.svg" alt="telefone icon">
+                                </label>
+                                <input type="text" id="phone" name="phone" placeholder="Telefone"/>
+                            </div>
+                        </div>
+                        <button>
+                            Confirmar
+                            <img src="./assets/arrow.svg" alt="Arrow right">
+                        </button>
+                    </form>
+                </section>
+            </main>
 `
 
     app.innerHTML = content //fazer com que o conteúdo que estava no html volte a aparecer pelo JS
+    app.setAttribute('class', 'page-start')
 
 
     formAction()
 }
 
 startApp ()
+
+document.querySelector("header").onclick = () => startApp()
